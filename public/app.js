2bfe508a885f529293df438d0944861b84c6e1cc
A'laa Mohamed Nosser <Nosser@MacBook-Pro.local>
2023-11-18 18:00:58 +0200

    inisial

--- public/app.js
+++ public/app.js
@@ -6,10 +6,12 @@ import cors from "cors";
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb+srv://nosseralaa7:wAhcvQpt0LQ0Nz1q@cluster0.nqs8bq6.mongodb.net/");
+mongoose.connect(
+  "mongodb+srv://nosseralaa7:wAhcvQpt0LQ0Nz1q@cluster0.nqs8bq6.mongodb.net/"
+);
 const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use("/apartments", apartmentRoutes);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
